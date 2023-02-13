615b8560d35c8cbc86c6d32069cd17203139d407
felipemaya <pipe.040691@gmail.com>
2023-02-13 17:59:45 -0500

    realizacion de slider con plugin de jQuery

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 1000,
+        auto: true,
+        stopAutoOnClick: true,
+        pager: true,
+    });
+    console.log("hola mundo");
+});
