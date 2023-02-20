506cbc9627514fde899e3d800d854d99006e5f3e
felipemaya <pipe.040691@gmail.com>
2023-02-20 05:48:26 -0500

    selector de temas

--- js/main.js
+++ js/main.js
@@ -33,11 +33,21 @@ $(document).ready(function(){
             title: 'prueba de titulo3',
             date: 'Publicado el '+moment().format('lll'),
             content: 'Lorem ipsum dolor sit amet, consectetur adipisicingelit. Possimus odio, impedit, earum facilis rerum nam omnis autem, expedita recusandae quo mollitia! Consectetur aut laboriosam, totam officiis inventore possimus aperiam eius? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aliquam facilis quam vero aliquid, officia veniam, voluptatibus harum, debitis qui quisquam laboriosam tempora ipsam excepturi natus exercitationem cumque fuga ad'
+        },
+        {
+            title: 'prueba de titulo5',
+            date: 'Publicado el '+moment().format('lll'),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicingelit. Possimus odio, impedit, earum facilis rerum nam omnis autem, expedita recusandae quo mollitia! Consectetur aut laboriosam, totam officiis inventore possimus aperiam eius? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aliquam facilis quam vero aliquid, officia veniam, voluptatibus harum, debitis qui quisquam laboriosam tempora ipsam excepturi natus exercitationem cumque fuga ad'
+        },
+        {
+            title: 'prueba de titulo6',
+            date: 'Publicado el '+moment().format('lll'),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicingelit. Possimus odio, impedit, earum facilis rerum nam omnis autem, expedita recusandae quo mollitia! Consectetur aut laboriosam, totam officiis inventore possimus aperiam eius? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aliquam facilis quam vero aliquid, officia veniam, voluptatibus harum, debitis qui quisquam laboriosam tempora ipsam excepturi natus exercitationem cumque fuga ad'
         }
     ];
 
    let div_posts = document.querySelector("#posts");
-
+    // esta funcion toma los datos del array de json y los maqueta para  luego ser insertado en la pagina
     posts.forEach(post=>{
 
        let articulo = `<article class="post">
@@ -50,5 +60,31 @@ $(document).ready(function(){
         </article> `;
         console.log(articulo);
         $("#posts").append(articulo);
-    })
+    });
+   
+
+
+    // codigo selector de temas para la  pagina
+    let theme = $("#theme"); // id del link que tiene el css del los colores del tema
+    if(localStorage.getItem("theme") !== null || localStorage.getItem("theme") !== undefined ){
+       theme.attr("href",localStorage.getItem("theme"));
+    }else{
+        localStorage.setItem("theme","css/green.css");
+    }
+
+    $("#to-green").click(function name(params) {
+        theme.attr("href", "css/green.css");
+        localStorage.setItem("theme","css/green.css");
+
+    });
+    $("#to-red").click(function name(params) {
+        theme.attr("href", "css/red.css");
+        localStorage.setItem("theme","css/red.css");
+        
+    });
+    $("#to-blue").click(function name(params) {
+        theme.attr("href", "css/blue.css");
+        localStorage.setItem("theme","css/blue.css");
+        
+    });
 });
